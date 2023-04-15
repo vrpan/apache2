8c18c88367329b0d94ee0e7631c2ea2b22d3353f
vrpan <109753535+vrpan@users.noreply.github.com>
2023-04-15 22:29:00 +0300

    Create apache2.sh

---
+++ apache2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install HTTP server
+sudo apt-get update
+sudo apt-get install apache2 -y
+
+# Read the log file
+LOG_FILE="/var/log/apache2/access.log"
+ERROR_LOG_FILE="/var/log/apache2/error.log"
+
+# Check for 4xx and 5xx errors in the log file
+if grep -q "HTTP/1.[01]\" [45][0-9][0-9]" "$LOG_FILE"; then
+    echo "4xx or 5xx errors found in the access log."
+else
+    echo "No 4xx or 5xx errors found in the access log."
+fi
+
+# Check for 4xx and 5xx errors in the error log file
+if grep -q "[45][0-9][0-9]" "$ERROR_LOG_FILE"; then
+    echo "4xx or 5xx errors found in the error log."
+else
+    echo "No 4xx or 5xx errors found in the error log."
+fi
